f7ba2a6846f7c0f1befe9b0755639f4543ae9f75
sanflat <tradeforce0720@gmail.com>
2023-09-14 17:53:46 +0900

    Update DB.sql ：CREATE DATABASE delicious_food_recipes AND CREATE TABLE [recipe/material/spices/cook/season/genre/review]

--- DB.sql
+++ DB.sql
@@ -1,1 +1,78 @@
-Hello World
+CREATE DATABASE delicious_food_recipes;
+USE delicious_food_recipes;
+
+CREATE TABLE recipe(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    cost INTEGER NULL,
+    calorie INTEGER NULL,
+    material_id INTEGER NOT NULL,
+    spices_id INTEGER NOT NULL,
+    cook_id INTEGER NOT NULL,
+    season_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL, 
+    review_id INTEGER NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE recipe_index (name)
+);
+
+CREATE TABLE material(
+    id INTEGER NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    amount VARCHAR(30) NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE material_index (name)
+);
+
+CREATE TABLE spices(
+    id INTEGER NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    amount VARCHAR(30) NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE spices_index (name)
+);
+
+CREATE TABLE cook(
+    id INTEGER NOT NULL,
+    order_num INTEGER NOT NULL,
+    memo text NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE cook_index (id, order_num)
+);
+
+CREATE TABLE season(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE season_index (name)
+);
+
+CREATE TABLE genre(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE genre_index (name)
+);
+
+CREATE TABLE review(
+    id INTEGER PRIMARY KEY,
+    reviewer_id INTEGER NOT NULL,
+    star_rating INTEGER NOT NULL,
+    comment text NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE review_index (id,reviewer_id)
+);
