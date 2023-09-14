ec8cea11d76bae5517a95829a6865716f2609d52
sanflat <tradeforce0720@gmail.com>
2023-09-14 09:47:53 +0900

    Update DB.sql ：CREATE TABLE recipe

--- DB.sql
+++ DB.sql
@@ -1,1 +1,19 @@
-Hello World
+CREATE DATABASE delicious_food_recipes;
+USE delicious_food_recipes;
+
+CREATE TABLE recipe(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    cost INTEGER NULL,
+    calorie INTEGER NULL,
+    material_id INTEGER NOT NULL,
+    spices_id INTEGER NOT NULL,
+    cook_id INTEGER NOT NULL,
+    season_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL, 
+    review_id INTEGER NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE recipe_index (name)
+);
