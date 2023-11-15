3da96180f94261224ea4d14a8532888738d721e8
sanflat <tradeforce0720@gmail.com>
2023-11-15 10:17:12 +0900

    update db/sql:テーブル追加（材料マスタと材料カテゴリ）

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE genre (
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     name character varying(30) NOT NULL,
     deleted_at timestamp without time zone,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE genre (
 );
 
 CREATE TABLE recipe (
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     name character varying(30) NOT NULL,
     breakfast_flg smallint,
     lunch_flg smallint,
@@ -23,9 +23,28 @@ CREATE TABLE recipe (
     updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE public.season (
-    id integer NOT NULL,
+CREATE TABLE season (
+    id SERIAL NOT NULL,
     name character varying(30) NOT NULL,
+    deleted_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE material_category (
+    id SERIAL NOT NULL,
+    name character varying(30) NOT NULL,
+    deleted_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE material_mst (
+    id SERIAL NOT NULL,
+    category_id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    search_text character varying(50) DEFAULT NULL,
+    price integer NOT NULL,
     deleted_at timestamp without time zone,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
